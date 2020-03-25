5d63dd8a9666d6e6aa2459f23da435def6307d0a
Felip3FL <felip3.fl@gmail.com>
2020-03-25 12:52:01 -0300

    Criação do controle NumerosLotofacil (principal)

---
+++ FL_TUR/FL_TUR/FL_TUR/Control/NumerosLotofacil.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FL_TUR.Control
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NumerosLotofacil : ContentView
+    {
+        public NumerosLotofacil()
+        {
+            InitializeComponent();
+            GridDemoPage();
+        }
+
+        public void GridDemoPage()
+        {
+            Grid grid = new Grid();
+            var contador = 1;
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    grid.Children.Add(new Button
+                    {
+                        Text = contador++.ToString()
+                    }, j, i);
+                }
+            }
+
+            // Accomodate iPhone status bar.
+            //this.Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
+
+            // Build the page.
+            this.Content = grid;
+        }
+
+    }
+}
