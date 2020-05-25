e5d449741f683ce2ea55bab3eb59028ba2092401
ssj4dofuturo <felip3.fl@gmail.com>
2020-05-25 10:11:36 -0300

    Add Model

---
+++ FL_TUR/FL_TUR/FL_TUR/Model/Numero.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FL_TUR.Model
+{
+    public class Numeros
+    {
+        List<int> numerosSorteados { get; set; }
+        List<int> numerosExcluidos { get; set; }
+    }
+}
