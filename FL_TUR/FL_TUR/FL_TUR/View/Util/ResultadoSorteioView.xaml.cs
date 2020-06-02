941d64f12d83cf345639cdfb7b606a80fefd12b2
ssj4dofuturo <felip3.fl@gmail.com>
2020-06-02 17:22:47 -0300

    Alterações para seguir padrão MVVM

--- FL_TUR/FL_TUR/FL_TUR/View/Util/ResultadoSorteioView.xaml.cs
+++ FL_TUR/FL_TUR/FL_TUR/View/Util/ResultadoSorteioView.xaml.cs
@@ -18,42 +18,7 @@ namespace FL_TUR.Control
         public ResultadoSorteioView()
         {
             InitializeComponent();
-            InitializeGrid();
-        }
-
-        private void InitializeGrid()
-        {
-            gridResultado = new Grid();
-            gridResultado.Style = (Style)Application.Current.Resources["ControlGridResultadoLotoFacil"];
-
-            var contadorClassId = 1;
-
-            for (int linha = 0; linha < 3; linha++) //3 Linhas
-            {
-                for (int coluna = 0; coluna < 5; coluna++) //5 Números por linhas
-                {
-                    gridResultado.Children.Add(new Label
-                    {
-                        Text = "-",
-                        ClassId = contadorClassId++.ToString(),
-                        Style = (Style)Application.Current.Resources["ControlLabelResultadoLotoFacil"]
-                }, coluna, linha); ;
-                }
-            };
-
-            this.Content = gridResultado;
-        }
-
-        public void ExibirResultadoTela(NumerosSorteadosClass numerosSorteadosClass)
-        {
-            var contador = 1;
-
-            foreach (var numeroSorteado in numerosSorteadosClass.getListNumerosSorteadosOrdeado())
-            {
-                var label = gridResultado.Children.Cast<Label>().Where(a => a.ClassId == contador.ToString()).FirstOrDefault();
-                label.Text = numeroSorteado.ToString("00");
-                contador++;
-            }
+            BindingContext = new ViewModel.ResultadoSorteioViewModel();
         }
     }
 }
--- FL_TUR/FL_TUR/FL_TUR/ViewModel/ResultadoSorteioViewModel.cs
+++ FL_TUR/FL_TUR/FL_TUR/ViewModel/ResultadoSorteioViewModel.cs
@@ -1,13 +1,42 @@
-﻿using System;
+﻿using FL_TUR.Control;
+using FL_TUR.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Text;
+using Xamarin.Forms;
 
 namespace FL_TUR.ViewModel
 {
     public class ResultadoSorteioViewModel : INotifyPropertyChanged
     {
+        public List<string> numeros { get; set; }
 
+        public ResultadoSorteioViewModel()
+        {
+            numeros = new List<string>();
+            for (int i = 0; i < 15; i++)
+            {
+                numeros.Add(i.ToString("00"));
+            }
+        }
+
+        public void ExibirResultadoTela(NumerosSorteadosClass numerosSorteadosClass)
+        {
+
+            
+
+            //var contador = 1;
+
+            //foreach (var numeroSorteado in numerosSorteadosClass.getListNumerosSorteadosOrdeado())
+            //{
+
+            //    grid.Children.Cast<Label>().Where(a => a.ClassId == contador.ToString()).FirstOrDefault();
+            //    label.Text = numeroSorteado.ToString("00");
+            //    contador++;
+            //}
+        }
 
         #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
